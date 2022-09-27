c8976b93907d7b2dc5b8e3c7e1e0446f96b44961
Rob Holland <rob@temporal.io>
2022-09-27 15:50:33 +0100

    Add summary dashboard.

--- dashboards/upsert.go
+++ dashboards/upsert.go
@@ -13,6 +13,14 @@ import (
 )
 
 func main() {
+	ctx := context.Background()
+	client := grabana.NewClient(&http.Client{}, os.Getenv("GRAFANA_HOST"), grabana.WithAPIToken(os.Getenv("GRAFANA_API_TOKEN")))
+
+	folder, err := client.FindOrCreateFolder(ctx, "Benchmarks")
+	if err != nil {
+		log.Fatalf("could not find or create folder: %v\n", err)
+	}
+
 	content, err := ioutil.ReadFile("./dashboards/stack.yaml")
 	if err != nil {
 		log.Fatalf("could not read file: %v\n", err)
@@ -23,15 +31,22 @@ func main() {
 		log.Fatalf("could not parse file: %v\n", err)
 	}
 
-	ctx := context.Background()
-	client := grabana.NewClient(&http.Client{}, os.Getenv("GRAFANA_HOST"), grabana.WithAPIToken(os.Getenv("GRAFANA_API_TOKEN")))
+	if _, err := client.UpsertDashboard(ctx, folder, dashboard); err != nil {
+		log.Fatalf("could not create dashboard: %v\n", err)
+	}
 
-	folder, err := client.FindOrCreateFolder(ctx, "Benchmarks")
+	content, err = ioutil.ReadFile("./dashboards/summary.yaml")
 	if err != nil {
-		log.Fatalf("could not find or create folder: %v\n", err)
+		log.Fatalf("could not read file: %v\n", err)
+	}
+
+	dashboard, err = decoder.UnmarshalYAML(bytes.NewBuffer(content))
+	if err != nil {
+		log.Fatalf("could not parse file: %v\n", err)
 	}
 
 	if _, err := client.UpsertDashboard(ctx, folder, dashboard); err != nil {
 		log.Fatalf("could not create dashboard: %v\n", err)
 	}
+
 }
